b6af803fe76ce9e3de8e279f6c6290b57b5b17f3
Hack Reactor Students <github@hackreactor.com>
2013-08-05 21:26:38 -0700

    refactored using _.each

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,41 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to have to write it from scratch:
+// 
+
+
+// this is what you would do if you liked things to be easy:
+// var stringifyJSON = JSON.stringify;
+
+// but you don't so you're going to have to write it from scratch:
+var stringifyJSON = function (obj) {
+  // your code goes here
+    if (typeof(obj) === 'string') {
+      return '"' + obj + '"';
+    }
+    else if (typeof(obj) === 'number' || typeof(obj) === 'boolean') {
+      return obj.toString();
+    }
+    else if (_.isArray(obj)) {
+      return '[' + _.map(obj, stringifyJSON).join(',') + ']';
+    }
+    else if (typeof(obj) === 'object') {
+      if (obj === null) {
+        return obj + '';
+      }
+      var arrstr = [];
+      _.each(obj, function(value, key) {
+         if (typeof(value) === 'undefined' || typeof(value) === 'function') {
+          return null;
+         }
+         arrstr.push(stringifyJSON(key) + ':' + stringifyJSON(value));
+      });
+      console.log('{' + arrstr.join(',')+ '}');
+      return ('{' + arrstr.join(',')+ '}');
+    }
+};
+
+/*
 var stringifyJSON = function (obj) {
   // your code goes here
     if (typeof(obj) === 'string') {
@@ -24,7 +59,12 @@ var stringifyJSON = function (obj) {
         }
       }).join(',') + '}';
     }
+
     else { //null
+    
       return obj + '';
     }
 };
+
+*/
+
