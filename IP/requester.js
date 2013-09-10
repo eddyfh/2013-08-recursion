a71a3c8790d1d92b3161bc8a4dabc8a8bbbaf339
Hack Reactor Students <github@hackreactor.com>
2013-09-09 22:06:09 -0700

    got append working

--- IP/requester.js
+++ IP/requester.js
@@ -1,12 +1,12 @@
 var fs = require('fs');
-var corsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10
-};
+// var corsHeaders = {
+//   'access-control-allow-origin': '*',
+//   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+//   'access-control-allow-headers': 'content-type, accept',
+//   'access-control-max-age': 10
+// };
 
-var headers = {"X-Originating-Ip": "199.87.82.66"};
+// var headers = {"X-Originating-Ip": "199.87.82.66"};
 var statusCode = 200;
 
 module.exports.handleRequest = function(req, res){
@@ -15,14 +15,14 @@ module.exports.handleRequest = function(req, res){
   if (req.url === '/' && req.method === 'GET'){
     console.log('its here');
     fs.readFile('test.html', function(err,text){
-      res.writeHead(statusCode, corsHeaders);
+      // res.writeHead(statusCode, corsHeaders);
       res.end(text);
     })
-  } else 
-  if (req.method === 'OPTIONS') {
-    res.writeHead(statusCode, corsHeaders);
-    res.end();
   } else {
+  // if (req.method === 'OPTIONS') {
+  //   res.writeHead(statusCode, corsHeaders);
+  //   res.end();
+  // } else {
     console.log('other');
   }
 
